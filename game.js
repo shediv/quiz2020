8301006ee7584660c9054baf067f0760edd40583
shediv <videshhedge@gmail.com>
2020-10-23 02:11:49 +0530

    Add files via upload

--- game.js
+++ game.js
@@ -4,24 +4,24 @@ var mythologyQuestions = [
       "category": "General Knowledge",
       "type": "multiple",
       "difficulty": "easy",
-      "question": "Who was the son of Bhima and Hidimba?",
-      "correct_answer": "Ghatotkacha",
+      "question": "If Christianity is the most popular religion (in terms of number of followers), where does Hinduism fall in the rankings?",
+      "correct_answer": "3rd",
       "incorrect_answers": [
-        "Jarasandha",
-        "Barbarik",
-        "Abhimanyu"
+        "5th",
+        "2nd",
+        "4th"
       ]
     },
     {
       "category": "General Knowledge",
       "type": "multiple",
       "difficulty": "easy",
-      "question": "Bali was killed by ______ .",
-      "correct_answer": "Ram",
+      "question": "The purpose of life is seen to be to gain liberation from the circle of re-incarnation by resolving Karma. What name is given to this achievement?",
+      "correct_answer": "Moksha",
       "incorrect_answers": [
-        "Ravana",
-        "Hanuman",
-        "Sugreev"
+        "Vedas",
+        "Sutras",
+        "Upanishads"
       ]
     },
     {
@@ -34,32 +34,248 @@ var mythologyQuestions = [
         ]
     },
     {
-        "question": "For how many days was the Battle of Kurukshetra fought?",
-        "correct_answer": "Ram",
+        "question": "Which of these Hindu festivals celebrates the victory of Rama over Ravana?",
+        "correct_answer": "Dusserah",
         "incorrect_answers": [
-          "Ravana",
-          "Hanuman",
-          "Sugreev"
+          "Mahashivarati",
+          "Ramnavami",
+          "Holi"
         ]
     },
     {
-        "question": "For how many days was the Battle of Kurukshetra fought?",
-        "correct_answer": "Ram",
+        "question": "Which Hindu deity is the Goddess of wealth and prosperity?",
+        "correct_answer": "Lakshmi",
         "incorrect_answers": [
-          "Ravana",
-          "Hanuman",
-          "Sugreev"
+          "Sita",
+          "Kurma",
+          "Saraswati"
         ]
     },
     {
-        "question": "For how many days was the Battle of Kurukshetra fought?",
-        "correct_answer": "Ram",
+        "question": "Which of these is widely recognized as the 8th incarnation of Vishnu?",
+        "correct_answer": "Krishna",
         "incorrect_answers": [
-          "Ravana",
-          "Hanuman",
-          "Sugreev"
+          "Kalki",
+          "Rama",
+          "Buddha"
         ]
     },
+    {
+      "question": "In Hinduism, who were seen as the first man and first woman?",
+      "correct_answer": "Manu and Shatrupa",
+      "incorrect_answers": [
+        "Adam and Eve",
+        "Kardam and Prasuti",
+        "Rama and Sita"
+      ]
+    },    
+    {
+      "question": "One of the Hindu myths is that those who commit sins go to hell or Naraka. Who is the lord of hell who decides the fate of people after their death?",
+      "correct_answer": "Yama",
+      "incorrect_answers": [
+        "Surya",
+        "Chitragupta",
+        "Rudra"
+      ]
+    },
+    {
+      "question": "In Hindu mythology, the crow is regarded as an inauspicious bird. What is the reason for this?",
+      "correct_answer": "Because the crow is the vehicle of Saturn",
+      "incorrect_answers": [
+        "Because the crow is wily in nature",
+        "Because of the crow's colour",
+        "Because the crow eats meat"
+      ]
+    },
+    {
+      "question": "Diwali or Deepavali is one of the Hindu festivals popular throughout the world. According to legend, why is this festival celebrated?",
+      "correct_answer": "To celebrate the return of Lord Rama to his capital Ayodhya",
+      "incorrect_answers": [
+        "To celebrate the day of the creation of the earth",
+        "To celebrate the killing of the demon Ravana by Lord Rama",
+        "To celebrate the victory of Lord Krishna over the evil Kamsa"
+      ]
+    },
+    {
+      "question": "Ravana was the king of a place that was probably which present day country?",
+      "correct_answer": "Sri Lanka",
+      "incorrect_answers": [
+        "United States of America",
+        "India",
+        "Australia"
+      ]
+    },
+    {
+      "question": "With which of the following states of India is the Garba dance associated?",
+      "correct_answer": "Gujarat",
+      "incorrect_answers": [
+        "Punjab",
+        "West Bengal",
+        "Haryana"
+      ]
+    },
+    {
+      "question": "What does the word 'ayurveda' literally mean?",
+      "correct_answer": "Science of life",
+      "incorrect_answers": [
+        "Healthcare",
+        "Science of studying Indian medicines",
+        "Curing diseases using Indian methods"
+      ]
+    },
+    {
+      "question": "An important exam's coming up, and I am really unprepared. I start praying to the Hindu goddess of knowledge to bless me so that I may pass. What is her name?",
+      "correct_answer": "Saraswati",
+      "incorrect_answers": [
+        "Urbashi",
+        "Yamuna",
+        "Ganga"
+      ]
+    },
+    {
+      "question": "The morning courier brings in happy news for Aditya. He has received the contract for construction of a multiplex. He bows his head down and prays to the Hindu god of workmanship and industry so that he may succeed. Who is Aditya praying to?",
+      "correct_answer": "Vishwakarma",
+      "incorrect_answers": [
+        "Shiva",
+        "Narasimha",
+        "Agni"
+      ]
+    },
+    {
+      "question": "Some farmers are really unhappy due to drought conditions. They keep on praying to the god of water to help them out. What is his name?",
+      "correct_answer": "Varundev",
+      "incorrect_answers": [
+        "Agnidev",
+        "Varundev",
+        "Vishwamitra"
+      ]
+    },
+    {
+      "question": "Before starting any special work, Aniruddha never forgets to pray to the Hindu god of success for blessings. What is the name of this god with whom the words 'Riddhi' and 'Siddhi' are often associated?",
+      "correct_answer": "Ganesha",
+      "incorrect_answers": [
+        "Mushak",
+        "Nisumbha",
+        "Sumbha"
+      ]
+    },
+    {
+      "question": "One of the Hindu festivals is celebrated by fasting throughout the day and staying awake at night in prayer. Which is this festival?",
+      "correct_answer": "Mahashivarathri",
+      "incorrect_answers": [
+        "Satyanarayan pooja",
+        "Gurupoornima",
+        "Mahalakshmi Pooja"
+      ]
+    },
+    {
+      "question": "Ramanavami is another important Hindu festival dedicated to the God Rama. Why is this festival celebrated?",
+      "correct_answer": "To mark the birth of Lord Rama",
+      "incorrect_answers": [
+        "To mark the marriage of Lord Rama to Sita",
+        "To mark the coronation of Lord Rama as King",
+        "To mark the victory of Lord Rama over Ravana"
+      ]
+    },
+    {
+      "question": "Janmashtami is a popular festival celebrated to mark the birth of a Hindu God. Which God is this?",
+      "correct_answer": "Krishna",
+      "incorrect_answers": [
+        "Surya",
+        "Rama",
+        "Yama"
+      ]
+    },
+    {
+      "question": "Who is the god of destruction?",
+      "correct_answer": "Lord Shiva",
+      "incorrect_answers": [
+        "Lord Vishnu",
+        "Lord Brahma",
+        "Lord Indra"
+      ]
+    },
+    {
+      "question": "What essential factor did Lakshman sacrifice during the 14 years of exile in an effort to guard Rama and Sita?",
+      "correct_answer": "Sleep",
+      "incorrect_answers": [
+        "Food",
+        "Communication",
+        "Looking at the sun"
+      ]
+    },
+    {
+      "question": "There are many deadly snakes in the world. Of the following list, what is the deadliest?",
+      "correct_answer": "Russell's Viper",
+      "incorrect_answers": [
+        "Inland Taipan",
+        "Green Mamba",
+        "Yellow Jawed Tommygoff"
+      ]
+    },
+    {
+      "question": "Some months have 30 days, some months have 31 days, but how many months have 28 days?",
+      "correct_answer": "12",
+      "incorrect_answers": [
+        "1",
+        "4",
+        "5"
+      ]
+    },
+    {
+      "question": "Divide 30 by half and add 10. What do you get?",
+      "correct_answer": "70",
+      "incorrect_answers": [
+        "20",
+        "25",
+        "50"
+      ]
+    },
+    {
+      "question": "When you know it takes five cats five minutes to catch five mice, how long will it take for 100 cats to catch 100 mice?",
+      "correct_answer": "5 minutes",
+      "incorrect_answers": [
+        "100 minutes",
+        "5 minutes",
+        "50 minutes"
+      ]
+    },
+    {
+      "question": "In indian mythology who was famous for his truthfulness?",
+      "correct_answer": "Harichandra",
+      "incorrect_answers": [
+        "Raja Bharat",
+        "Raja Dashrat",
+        "Lord Ram"
+      ]
+    },
+    {
+      "question": "In Christianity what does Amen mean?",
+      "correct_answer": "So be it.",
+      "incorrect_answers": [
+        "Thanks God",
+        "Please be with us",
+        "Lord is great"
+      ]
+    },
+    {
+      "question": "Name the Scandinavian God of thunder and war?",
+      "correct_answer": "Thor",
+      "incorrect_answers": [
+        "Thanos",
+        "Superman",
+        "Loki"
+      ]
+    },
+    {
+      "question": "Which is considered as the Hindu mythological lord of birds?",
+      "correct_answer": "Garuda",
+      "incorrect_answers": [
+        "Eagle",
+        "Parrot",
+        "Peackock"
+      ]
+    }
 ]
 
 var historyQuestions = [   
@@ -89,45 +305,122 @@ var historyQuestions = [
     }
 ]
 
-var politicsQuestions = [   
+var politicsQuestions = [ 
     {
-      "category": "General Knowledge",
-      "type": "multiple",
-      "difficulty": "easy",
-      "question": "What machine element is located in the center of fidget spinners?",
-      "correct_answer": "Bearings",
+      "question": "The members of the Rajya Sabha are elected by",
+      "correct_answer": "elected members of the legislative assembly",
       "incorrect_answers": [
-        "Axles",
-        "Gears",
-        "Belts"
+        "the people",
+        "Lok Sabha",
+        "elected members of the legislative assembly",
+        "elected members of the legislative council"
       ]
     },
     {
-      "category": "General Knowledge",
-      "type": "multiple",
-      "difficulty": "easy",
-      "question": "The drug cartel run by Pablo Escobar originated in which South American city?",
-      "correct_answer": "Medell&iacute;n",
+      "question": "The members of Lok Sabha hold office for a term of",
+      "correct_answer": "5y",
       "incorrect_answers": [
-        "Bogot&aacute;",
-        "Quito",
-        "Cali"
+        "4y", "3y", "6y"
+      ]
+    },
+    {
+      "question": "The minimum age to qualify for election to the Lok Sabha is",
+      "correct_answer": "25y",
+      "incorrect_answers": [
+        "21y", "18y", "35y"
+      ]
+    },
+    {
+      "question": "The minimum number of members that must be present to hold the meeting of the Lok Sabha is",
+      "correct_answer": "one-tenth of the total membership of the Houses",
+      "incorrect_answers": [
+        "one-fourth of the total membership", "50 percent strength of the Lok Sabha", "at least 100 members"
+      ]
+    },
+    {
+      "question": "The members of Parliament can express themselves in the House in",
+      "correct_answer": "English, Hindi or mother tongue",
+      "incorrect_answers": [
+        "English only", "Hindi only", "English or Hindi"
+      ]
+    },
+    {
+      "question": "The national anthem was written by",
+      "correct_answer": "Rabindranath Tagore",
+      "incorrect_answers": [
+        "Bankim Chandra Chatterjee", "Sarat Chandra Chatterjee", "Aurobindo Ghosh"
+      ]
+    },
+    {
+      "question": "The minimum age required to become the prime minister of India is",
+      "correct_answer": "25y",
+      "incorrect_answers": [
+        "30y", "45y", "35y"
+      ]
+    },
+    {
+      "question": "The members of the State Public Service Commission are appointed by the",
+      "correct_answer": "governor",
+      "incorrect_answers": [
+        "chief minister", "chief justice", "vice-president"
+      ]
+    },
+    {
+      "question": "Who is CEO Facebook",
+      "correct_answer": "Mark Zuckerberg",
+      "incorrect_answers": [
+        "", "", ""
+      ]
+    },
+    {
+      "question": "Prime Minister of India",
+      "correct_answer": "Narendra Modi",
+      "incorrect_answers": [
+        "", "", ""
+      ]
+    },
+    {
+      "question": "Minister of Railways",
+      "correct_answer": "Piyush Goyal",
+      "incorrect_answers": [
+        "Ravi Shankar Prasad", "Ramesh Pokhriyal", "Narendra Singh Tomar"
+      ]
+    },
+    {
+      "question": "Minister of Home Affairs",
+      "correct_answer": "Amit Shah",
+      "incorrect_answers": [
+        "", "", ""
+      ]
+    },
+    {
+      "question": "Minister of Town & Country Planning, Agriculture of goa",
+      "correct_answer": "Chandrakant Kavalekar",
+      "incorrect_answers": [
+        "", "", ""
+      ]
+    },
+    {
+      "question": "Minister of Waste Management, Science & Technology of goa",
+      "correct_answer": "Michael Lobo",
+      "incorrect_answers": [
+        "", "", ""
+      ]
+    },
+    {
+      "question": "Minister of Power, Environment of goa",
+      "correct_answer": "Nilesh Cabral",
+      "incorrect_answers": [
+        "", "", ""
       ]
     }
 ]
 
 var sportsQuestions = [   
     {
-      "category": "General Knowledge",
-      "type": "multiple",
-      "difficulty": "easy",
       "question": "What machine element is located in the center of fidget spinners?",
       "correct_answer": "Bearings",
-      "incorrect_answers": [
-        "Axles",
-        "Gears",
-        "Belts"
-      ]
+      "incorrect_answers": ["", "", ""]
     },
     {
       "category": "General Knowledge",
